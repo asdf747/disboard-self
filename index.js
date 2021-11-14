60574a549064cb3571fb8c165c9ed80dfc8b5a97
ADG <65923257+andrelucaas@users.noreply.github.com>
2021-11-14 09:45:56 -0300

    Arrumar identação

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 // Configuration Files
 const config = require('./config');
 // Creating the Client
-const { Client } = require('discord.js-self');
+const {
+    Client
+} = require('discord.js-self');
 const client = new Client();
 
 client.on('ready', async () => {
-  		const CronJob = require('cron').CronJob;
-  		const bump = new CronJob('0 * */2 * * *', function() {
+    const CronJob = require('cron').CronJob;
+    const bump = new CronJob('0 * */2 * * *', function() {
         const canal = client.channels.cache.get(config.channel);
         canal.send('!d bump');
-		}, null, !0, 'America/Sao_Paulo');
+    }, null, !0, 'America/Sao_Paulo');
 })
 
 client.login(config.token);
